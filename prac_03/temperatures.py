61974a0e7a32e1a31907a11dd435e0c7bb99048d
zacgilby <zachary.gilby@my.jcu.edu.au>
2018-08-21 15:57:24 +1000

    Refactor celsius and fahrenheit calculations

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -13,11 +13,11 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = convert_celsius_to_fahrenheit(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print("Result: {:.2f} C".format(celsius))
         else:
             print("Invalid option")
@@ -25,6 +25,13 @@ def main():
         choice = input(">>> ").upper()
     print("Thank you.")
 
+
+def convert_celsius_to_fahrenheit(celsius):
+    return celsius * 9.0 / 5 + 32
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return 5 / 9 * (fahrenheit - 32)
 
 
 main()
