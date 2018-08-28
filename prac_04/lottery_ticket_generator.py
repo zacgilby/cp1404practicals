1e1d146a355928f9e220f672745c2f302ba468ea
zacgilby <zachary.gilby@my.jcu.edu.au>
2018-08-28 11:42:42 +1000

    Improved code layout and docstring

--- prac_04/lottery_ticket_generator.py
+++ prac_04/lottery_ticket_generator.py
@@ -1,9 +1,9 @@
 """
 CP1404/CP5632 - Practical
-Randomly generates quick picks.
+Randomly generates a quick pick game chosen by the user..
 """
 
-import random
+from random import randint
 
 NUMBERS_PER_LINE = 6
 MINIMUM = 1
@@ -13,15 +13,18 @@ MAXIMUM = 45
 def main():
     number_of_quick_picks = int(input("How many quick picks? "))
     while number_of_quick_picks < 0:
-        print("Come on, at least one!")
+        print("Come on, at least one game!")
         number_of_quick_picks = int(input("How many quick picks? "))
+    generate_quick_pick(number_of_quick_picks)
 
+
+def generate_quick_pick(number_of_quick_picks):
     for i in range(number_of_quick_picks):
         quick_pick = []
         for j in range(NUMBERS_PER_LINE):
-            number = random.randint(MINIMUM, MAXIMUM)
+            number = randint(MINIMUM, MAXIMUM)
             while number in quick_pick:
-                number = random.randint(MINIMUM, MAXIMUM)
+                number = randint(MINIMUM, MAXIMUM)
             quick_pick.append(number)
         quick_pick.sort()
         print(" ".join("{:2}".format(number) for number in quick_pick))
