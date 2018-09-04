adb835ef1544eaa5687af3222dbc8d0f20ada9c3
zacgilby <zachary.gilby@my.jcu.edu.au>
2018-09-04 15:47:23 +1000

    add guitars.py

--- guitars.py
+++ guitars.py
@@ -19,7 +19,9 @@ while name != "":
 
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
 # TODO: TypeError when exiting while loop, find solution since the solution doesn't work.
+
 guitars.sort()
 print("These are my guitars:")
 
